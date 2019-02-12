3216d3e839718471920b09b9a52e5623b5f0d57e
akshatcx <akshat.c2k@gmail.com>
2019-02-12 21:09:32 +0530

    [Addition]
    -Created a template for movieInfo page
    -Added .vscode/settings.json
    
    [Modification]
    
    -Modified the controller file by adding a new route
    -Modified the bse.html.j2

--- app/home/controller.py
+++ app/home/controller.py
@@ -1,5 +1,5 @@
 from flask import (Blueprint, request, render_template,
-                  flash, g, session, redirect, url_for, jsonify)
+                  flash, g, session, redirect, url_for, jsonify, abort)
 from app import db, models
 import app.home.helper as helper
 
@@ -14,3 +14,15 @@ def home_route():
 		'data': db.movies.find()
 	}
 	return render_template('home/index.html.j2', **context_kwargs)
+
+
+@home.route('/movies/<imdbid>', methods = ['GET'])
+def mov_info(imdbid):
+	movie = db.movies.find_one( {"imdbID" : imdbid})
+	if movie is None:
+		abort(404) 
+	context_kwargs = {
+		'title': "MovieInfo",
+		'movie' : movie,
+	}
+	return render_template('home/movinfo.html.j2', **context_kwargs)
